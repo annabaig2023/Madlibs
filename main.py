c3ce549d9c00849d91bb4acd8edf51925e23f413
annabaig2023 <78822497+annabaig2023@users.noreply.github.com>
2021-08-16 00:40:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# string concatenation
+swe = "Anna Baig"
+
+print (swe + " likes to code")
+# print (f"{swe} likes to code")
+# print ("{} likes to code".format(swe))
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = ("Computer programming is so " + adj + "! It makes me so excited all the time because I love to " + verb1 + "! Stay hydrated and " + verb2 + " like you are " + famous_person)
+
+print(madlib)
+
+
